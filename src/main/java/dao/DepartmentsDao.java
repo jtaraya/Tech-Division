5abbd0ed468f55b613161ae54fbcb895f620e9de
jtaraya <jacobtaraya@gmail.com>
2022-07-22 13:19:06 +0300

    updated Department Dao

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -7,4 +7,17 @@ import java.util.List;
 public interface DepartmentsDao {
     //List
     List<Departments> getAll();
+
+    //Departments category
+    void add (Departments category);
+
+    //Read
+    Departments findById(int id);
+
+    //update
+    void update(int id, String name);
+
+    //Delete
+    void deleteById(int id);
+    void deleteAllDepartments();
 }
